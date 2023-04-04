4d4720f8c0d3490724fa399cdfb479aa40a8bb18
梁振盛 <liangzs@yingmi.cn>
2023-04-04 22:12:52 +0800

    init: rspack antd-mobile-v5报错例子

---
+++ rspack.config.js
@@ -0,0 +1,51 @@
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+
+console.log("process.end.NODE_ENV", process.env.NODE_ENV);
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    main: "./src/main.jsx",
+  },
+  builtins: {
+    html: [
+      {
+        template: "./index.html",
+      },
+    ],
+    target: ["web"],
+    presetEnv: {
+      mode: "entry",
+      targets: ["Android >= 5.1"],
+      coreJs: "3",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        type: 'jsx', // https://www.rspack.dev/zh/guide/react.html
+      },
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+      {
+        test: /.less$/,
+        use: [
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+        type: "css",
+      },
+    ],
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { ActionSheet, Button } from "antd-mobile";
+
+const actions = [
+  { text: "复制", key: "copy" },
+  { text: "修改", key: "edit" },
+  { text: "保存", key: "save" },
+];
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Rspack + React</h1>
+      <div className="card">
+        <Button color="primary" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </div>
+
+      {/* 出bug的组件 */}
+      <ActionSheet
+        extra="请选择你要进行的操作"
+        visible={true}
+        cancelText="取消"
+        actions={actions}
+        closeOnMaskClick={false}
+        popupClassName="action-sheet-common"
+        onClose={() => setIsActionSheetShow(false)}
+      />
+    </div>
+  );
+}
+
+export default App;
